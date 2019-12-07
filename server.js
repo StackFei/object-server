b395873f0ad5cf3696441bfdf504c97be3e37bc4
pengyunfei <stackfei@gmail.com>
2019-12-07 16:32:51 +0800

    saves

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ let http = require("http");
 let users = [
   { id: 1, name: "6666" },
   { id: 2, name: "7777" },
-  { id: 3, name: "8888" }
+  { id: 2, name: "8888" },
+  { id: 2, name: "999" },
+  { id: 2, name: "53" },
+  { id: 2, name: "54" },
+  { id: 2, name: "232" },
 ];
 let server = http.createServer(function(req, res) {
   console.log(req.method, req.url);
